1373f1db3aacbd9696c823f8939023df3b020071
heyiming <ymjrcc@qq.com>
2024-09-27 13:04:23 +0800

    Create New Campaign

--- src/app/crowd-fund/page.tsx
+++ src/app/crowd-fund/page.tsx
@@ -1,13 +1,15 @@
 'use client'
-import { useEffect, useState } from "react";
-import { parseEther } from "viem";
+import { useEffect, useMemo, useState } from "react";
+import { parseEther, formatUnits } from "viem";
+import toast from "react-hot-toast";
 import {
   Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure,
   Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, 
-  Link, Button, Input, DatePicker
+  Link, Button, Input, DatePicker, Code
  } from "@nextui-org/react";
  import { now, getLocalTimeZone } from "@internationalized/date";
-import { useWriteCrowdFund, useReadCrowdFund } from "@/utils/contracts";
+ import { useReadContracts, useWaitForTransactionReceipt } from 'wagmi';
+import { useWriteCrowdFund, useReadCrowdFund, crowdFundAbi } from "@/utils/contracts";
 
 const contractAddress = '0x0165878a594ca255338adfa4d48449f69242eb8f' as const
 const erc20Address = '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707' as const
@@ -19,21 +21,65 @@ const info = [
   { key: 'CFT (ERC20)', value: erc20Address, href: `https://sepolia.etherscan.io/address/${erc20Address}` },
 ]
 
+const formatAddress = (address: string) => {
+  return address.slice(0, 6) + '...' + address.slice(-4)
+}
+
 const Page = () => {
   const [goal, setGoal] = useState('')
+  const [rows, setRows] = useState<any[]>([])
   const [startTime, setStartTime] = useState(now(getLocalTimeZone()).add({hours: 1}))
   const [endTime, setEndTime] = useState(now(getLocalTimeZone()).add({hours: 1, weeks: 1}))
   const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
   const { data: hash, writeContractAsync: writeCrowdFund, isPending } = useWriteCrowdFund()
-  const { data: campaigns } = useReadCrowdFund({
+  const { isLoading, isSuccess } = useWaitForTransactionReceipt({ hash });
+
+  const { data: count, refetch: refetchCount } = useReadCrowdFund({
     address: contractAddress,
-    functionName: 'campaigns',
-    args: [BigInt(1)]
+    functionName: 'count',
+  })
+
+  const list = useMemo(() => {
+    const _list = Array.from({ length: Number(count) || 0 }, (_, i) => i + 1)
+    return _list
+  }, [count])
+
+  const { data: campaigns, refetch: refetchCampaigns } = useReadContracts({
+    contracts: list.map((id) => ({
+      address: contractAddress,
+      functionName: 'campaigns',
+      abi: crowdFundAbi,
+      args: [BigInt(id)]
+    }))
   })
 
   useEffect(() => {
-    console.log('Campaigns:', campaigns)
+    if(list.length > 0) {
+      refetchCampaigns()
+    }
+  }, [list])
+
+  useEffect(() => {
+    const _rows = campaigns?.map((campaign: any, index: number) => {
+      if(campaign.status === 'success' && campaign.result) {
+        return {
+          id: index + 1,
+          creator: campaign.result[0],
+          goal: formatUnits(campaign.result[1], 18),
+          pledged: formatUnits(campaign.result[2], 18),
+          claimed: campaign.result[5],
+        }
+      }
+      return null
+    }).filter((row) => row !== null)
+    setRows(_rows || [])
   }, [campaigns])
+
+  useEffect(() => {
+    if(!isPending) {
+      refetchCount()
+    }
+  }, [isPending])
 
   const onLaunch = async () => {
 
@@ -45,8 +91,29 @@ const Page = () => {
       address: contractAddress,
       functionName: 'launch',
       args: [_goal, _startTime, _endTime],
+    },{
+      onSuccess: (data) => {
+        console.log('success', data)
+        toast.success('Launch success')
+        onClose()
+      },
+      onError: (error) => {
+        toast.error(error.message, {
+          style: {
+            wordBreak: 'break-all'
+          }
+        })
+      }
     })
   }
+
+  const onCreate = () => {
+    setGoal('')
+    setStartTime(now(getLocalTimeZone()).add({hours: 1}))
+    setEndTime(now(getLocalTimeZone()).add({hours: 1, weeks: 1}))
+    onOpen()
+  }
+
   return (
     <>
       <ul className="bg-gray-50 rounded-xl px-4 py-2 mb-4">
@@ -66,8 +133,8 @@ const Page = () => {
         }
       </ul>
       <div className="flex justify-between items-center">
-        <div className='text-xl font-bold  text-gray-400 mb-2 mt-4'>Crowd Fund List:</div>
-        <Button size="sm" color="primary" variant="flat" onClick={onOpen}>Launch New Crowd Fund</Button>
+        <div className='text-xl font-bold  text-gray-400 mb-2 mt-4'>Campaigns List:</div>
+        <Button size="sm" color="primary" variant="flat" onClick={onCreate}>Create New Campaign</Button>
       </div>
       <Table aria-label="Example static collection table">
         <TableHeader>
@@ -75,51 +142,31 @@ const Page = () => {
           <TableColumn>Creator</TableColumn>
           <TableColumn>Goal</TableColumn>
           <TableColumn>Pledged Amount</TableColumn>
-          <TableColumn>Status</TableColumn>
+          <TableColumn>Claimed</TableColumn>
           <TableColumn>Action</TableColumn>
         </TableHeader>
         <TableBody>
-          <TableRow key="1">
-            <TableCell>Tony Reichert</TableCell>
-            <TableCell>CEO</TableCell>
-            <TableCell>Active</TableCell>
-            <TableCell>Active</TableCell>
-            <TableCell>Active</TableCell>
-            <TableCell>
-              <Link href="/crowd-fund/1">Detail</Link>
-            </TableCell>
-          </TableRow>
-          <TableRow key="2">
-            <TableCell>Zoey Lang</TableCell>
-            <TableCell>Technical Lead</TableCell>
-            <TableCell>Paused</TableCell>
-            <TableCell>Paused</TableCell>
-            <TableCell>Paused</TableCell>
-            <TableCell>Paused</TableCell>
-          </TableRow>
-          <TableRow key="3">
-            <TableCell>Jane Fisher</TableCell>
-            <TableCell>Senior Developer</TableCell>
-            <TableCell>Active</TableCell>
-            <TableCell>Active</TableCell>
-            <TableCell>Active</TableCell>
-            <TableCell>Active</TableCell>
-          </TableRow>
-          <TableRow key="4">
-            <TableCell>William Howard</TableCell>
-            <TableCell>Community Manager</TableCell>
-            <TableCell>Vacation</TableCell>
-            <TableCell>Vacation</TableCell>
-            <TableCell>Vacation</TableCell>
-            <TableCell>Vacation</TableCell>
-          </TableRow>
+          {
+            rows.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>{row.id}</TableCell>
+                <TableCell><Code>{formatAddress(row.creator)}</Code></TableCell>
+                <TableCell>{row.goal}</TableCell>
+                <TableCell>{row.pledged}</TableCell>
+                <TableCell>{row.claimed.toString()}</TableCell>
+                <TableCell>
+                  <Link href={`/crowd-fund/${row.id}`}>Detail</Link>
+                </TableCell>
+              </TableRow>
+            ))
+          }
         </TableBody>
       </Table>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">Launch New Crowd Fund</ModalHeader>
+              <ModalHeader className="flex flex-col gap-1">Create New Campaign</ModalHeader>
               <ModalBody>
                 <Input 
                   type="number" variant="bordered" label="Goal" 
@@ -149,7 +196,7 @@ const Page = () => {
                 </Button>
                 <Button 
                   color="primary" onPress={onLaunch}
-                  isLoading={isPending}
+                  isLoading={isPending || isLoading}
                 >
                   Submit
                 </Button>
